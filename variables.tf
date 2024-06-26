0b0294a1a5f1512aa50fb9476cf7298301896432
Shiva <shivatechnology23@gmail.com>
2024-06-27 03:32:05 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  type = string
+  default = "webx-424701"
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "my-gke-cluster"
+}
